b5b0921c43c7a4e535541c033ba0b81be0205b28
belong <33158742@qq.com>
2025-11-24 19:10:53 +0800

    refactor:  更新基本示例代码

--- src/pages/01_simple-formily/index.tsx
+++ src/pages/01_simple-formily/index.tsx
@@ -1,44 +1,85 @@
-import { Input, Select, Form, FormItem, FormGrid, FormButtonGroup, Submit } from '@formily/antd-v5'
+import { Input, Select, FormItem, FormButtonGroup, Submit, Form, Reset } from '@formily/antd-v5'
 import { createForm } from '@formily/core'
-import { Field } from '@formily/react'
+import { createSchemaField } from '@formily/react'
+
+const Title = (props) => <h3>{props.text}</h3>
+
+const SchemaField = createSchemaField({
+  components: {
+    Input,
+    Select,
+    FormItem,
+    Title,
+  },
+})
 
 const form = createForm()
-const Demo: React.FC = () => {
-  return (
-    <Form
-      form={form}
-      layout="vertical"
-      feedbackLayout="terse"
-      onAutoSubmit={console.log}
-      onAutoSubmitFailed={console.log}
-    >
-      <FormGrid maxColumns={4}>
-        <Field
-          name="aa"
-          title="选择框"
-          decorator={[FormItem]}
-          component={[Select]}
-          dataSource={[
-            {
-              label: '选项1',
-              value: 1,
-            },
-            {
-              label: '选项2',
-              value: 2,
-            },
-          ]}
-        />
-        <Field name="bb" title="输入框" required decorator={[FormItem]} component={[Input]} />
-        <Field name="cc" title="输入框" decorator={[FormItem]} component={[Input]} />
-        <Field name="dd" title="输入框" decorator={[FormItem]} component={[Input]} />
-        <Field name="ee" title="输入框" decorator={[FormItem]} component={[Input]} />
-        <FormButtonGroup.FormItem>
-          <Submit>查询</Submit>
-        </FormButtonGroup.FormItem>
-      </FormGrid>
-    </Form>
-  )
-}
 
-export default Demo
+export default () => (
+  <Form
+    form={form}
+    onAutoSubmit={(formValues) => {
+      console.log('values:', formValues)
+    }}
+  >
+    <SchemaField>
+      {/* 输入框 */}
+      <SchemaField.String
+        name="goodName"
+        title="商品名称"
+        x-decorator="FormItem"
+        x-component="Input"
+        x-decorator-props={{
+          labelWidth: 100,
+          labelAlign: 'right',
+          tooltip: '提示提示',
+          tooltipLayout: 'text',
+          wrapperWidth: 300,
+          colon: false, // 不展示冒号，默认为true
+        }}
+        x-component-props={{
+          placeholder: '请输入',
+          style: {
+            width: 240,
+          },
+        }}
+      />
+
+      {/* 输入框 */}
+      <SchemaField.String name="goodPrice" title="商品价格" x-decorator="FormItem" x-component="Input" />
+
+      {/* 下拉框 */}
+      <SchemaField.String
+        name="goodStatus"
+        title="商品状态"
+        x-decorator="FormItem"
+        x-component="Select"
+        enum={[
+          { label: '选项1', value: 1 },
+          { label: '选项2', value: 2 },
+        ]}
+      />
+
+      {/* 单选框 */}
+      <SchemaField.String
+        name="size"
+        title="Radio.Group"
+        x-decorator="FormItem"
+        x-component="Radio.Group"
+        enum={[
+          { value: 'small', label: 'Small' },
+          { value: 'default', label: 'Default' },
+          { value: 'large', label: 'Large' },
+        ]}
+      />
+
+      {/* 自定义组件 */}
+      <SchemaField.Void x-component="Title" x-component-props={{ text: '我是自定义组件要展示的内容' }} />
+    </SchemaField>
+
+    <FormButtonGroup>
+      <Reset>重置</Reset>
+      <Submit>提交</Submit>
+    </FormButtonGroup>
+  </Form>
+)
