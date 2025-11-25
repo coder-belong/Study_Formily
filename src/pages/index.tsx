2191c93ebe6fabb3df6a82fcab886ddea61e8c6f
belong <33158742@qq.com>
2025-11-25 20:43:45 +0800

    feat: add Formily dependent demo with form state management and update main app layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import SimpleDemo from '@/components/01-formily基本使用'
+import FormilyDependentDemo from '@/components/02_formily表单依赖联动'
 
-import { Card } from 'antd'
+import { Card, Flex } from 'antd'
 
 const App: React.FC = () => {
   return (
-    <>
+    <Flex vertical gap={16}>
       <Card title="Formily基本使用">
         <SimpleDemo />
       </Card>
-    </>
+
+      <Card title="Formily表单依赖联动">
+        <FormilyDependentDemo />
+      </Card>
+    </Flex>
   )
 }
 
